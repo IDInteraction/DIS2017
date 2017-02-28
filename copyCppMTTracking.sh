75e1aa96f310871b7ebcf977f1c509eb06ecc474
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-02-28 09:48:24 +0000

    Copy and rename corrected version of tracking files (startframes were slightly off for some partitipants)

--- copyCppMTTracking.sh
+++ copyCppMTTracking.sh
@@ -9,11 +9,11 @@ echo $participants
 for p in $participants; do
 	echo Copying participant: $p;
 
-	cp ../spot_the_difference/cppmtTracking/${p}_front.csv  ./Tracking/${p}_part1_front_cppMT.csv;
+	cp ./TrackingV2/cppMTPart1/${p}_front.csv  ./Tracking/${p}_part1_front_cppMT.csv;
 	gzip ./Tracking/${p}_part1_front_cppMT.csv;
 
 
-	cp ../spot_the_difference/cppmtTrackingPart2/${p}_front.csv  ./Tracking/${p}_part2_front_cppMT.csv;
+	cp ./TrackingV2/cppMTPart2/${p}_front.csv  ./Tracking/${p}_part2_front_cppMT.csv;
 	gzip ./Tracking/${p}_part2_front_cppMT.csv;
 
 done;
